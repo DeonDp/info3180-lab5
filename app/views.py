138c9f90294d3b1e1655951ff8c990cd3aa3de17
DeonDp <deongohard@hotmail.com>
2017-02-22 11:38:46 +0000

    step8

--- app/views.py
+++ app/views.py
@@ -6,10 +6,11 @@ This file creates your application.
 """
 
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from flask_login import login_user, logout_user, current_user, login_required
 from forms import LoginForm
 from models import UserProfile
+
 
 
 ###
@@ -32,19 +33,25 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username=username, password=password).first()
             # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
+            if user is not None:
+                login_user(user)
+    
+                # remember to flash a message to the user
+                flash('Log in successful.')
+                return redirect(url_for("secure_page")) # they should be redirected to a secure-page route instead
+            else:
+                flash('Username or Password is incorrect.')
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
